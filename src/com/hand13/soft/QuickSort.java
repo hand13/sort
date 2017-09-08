796e33d4003f64f2444a6284d7b06e30e9be326d
hand13 <1101738345@qq.com>
2017-09-09 04:06:52 +0800

    quicksort

--- src/com/hand13/soft/QuickSort.java
+++ src/com/hand13/soft/QuickSort.java
@@ -4,4 +4,32 @@ package com.hand13.soft;
  * Created by hd110 on 2017/9/9.
  */
 public class QuickSort {
+    public static int sort(int[]a, int start, int end) {
+        if(start >= end)
+            return -1;
+        int i = start - 1;
+        for (int j = start; j <= end; j++) {
+            if(a[j] <= a[end]) {
+                i++;
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+        return i;
+    }
+    public static void quicksort(int []a,int start, int end) {
+        int mid = sort(a,start,end);
+        if(mid == -1)
+            return;
+        quicksort(a,start,mid - 1);
+        quicksort(a,mid+1,end);
+    }
+    public static void main(String[] args) {
+        int[] a= {4,3,6,5,7,1};
+        quicksort(a,0,a.length-1);
+        for(int i : a) {
+            System.out.println(i);
+        }
+    }
 }
